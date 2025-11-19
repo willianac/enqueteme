0395fc2d9afd2c28b2f9da7824990b4cd9c03b15
Willian Almeida <willianac01@gmail.com>
2025-11-19 13:51:34 -0300

    feat: create 'CreatePoll' endpoint

--- src/main/java/com/will/enqueteme/controllers/EnqueteController.java
+++ src/main/java/com/will/enqueteme/controllers/EnqueteController.java
@@ -1,10 +1,19 @@
 package com.will.enqueteme.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.will.enqueteme.dto.CreatePollDTO;
+import com.will.enqueteme.models.Enquete;
+import com.will.enqueteme.models.Opcao;
 import com.will.enqueteme.repositories.EnqueteRepository;
 
 @RestController
@@ -13,7 +22,37 @@ public class EnqueteController {
     @Autowired
     private EnqueteRepository enqueteRepository;
 
+    @GetMapping("")
     public ResponseEntity<?> getAllPolls() {
         return ResponseEntity.ok(enqueteRepository.findAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> createPoll(@RequestBody CreatePollDTO createPollDTO) {
+        if(createPollDTO.getTitle() == null || createPollDTO.getTitle().isEmpty() ||
+           createPollDTO.getOptions() == null || createPollDTO.getOptions().length < 2) {
+            return ResponseEntity.badRequest().body("Invalid poll data. Title and at least two options are required.");
+        }
+
+        try {
+            Enquete enquete = new Enquete();
+            enquete.setTitle(createPollDTO.getTitle());
+
+            List<Opcao> opcoes = Arrays.stream(createPollDTO.getOptions())
+                .map(optionTitle -> {
+                    Opcao opcao = new Opcao();
+                    opcao.setName(optionTitle);
+                    opcao.setEnquete(enquete);
+                    return opcao;
+                })
+                .toList();
+
+            enquete.setOpcoes(opcoes);
+            enqueteRepository.save(enquete);
+            return ResponseEntity.created(null).body(enquete);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(500).body("An error occurred while creating the poll.");
+        }
     }
 }
