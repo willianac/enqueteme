ad85bd36cb6c89c4c3dc6948a8112c65cbed93bb
Willian Almeida <willianac01@gmail.com>
2025-11-19 13:51:18 -0300

    fix: create Opcao repository

---
+++ src/main/java/com/will/enqueteme/repositories/OpcaoRepository.java
@@ -0,0 +1,9 @@
+package com.will.enqueteme.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.will.enqueteme.models.Opcao;
+
+public interface OpcaoRepository extends CrudRepository<Opcao, Long> {
+    
+}
