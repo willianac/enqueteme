ee99c6f062a9361d6f4e9c26996ec86e410d7e88
Willian Almeida <willianac01@gmail.com>
2025-11-24 15:33:00 -0300

    feat: new-poll component now is dynamic with reactive form

--- web/src/app/features/polls/components/new-poll/new-poll.ts
+++ web/src/app/features/polls/components/new-poll/new-poll.ts
@@ -4,13 +4,40 @@ import { TuiCardLarge } from '@taiga-ui/layout';
 import { TuiPlatform } from '@taiga-ui/cdk';
 import { TuiButton, TuiTextfield, TuiAppearance } from '@taiga-ui/core';
 import { TuiButtonClose } from '@taiga-ui/kit';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-poll',
-  imports: [Navbar, TuiCardLarge, TuiPlatform, TuiButton, TuiTextfield, TuiAppearance, TuiButtonClose],
+  imports: [
+    Navbar, 
+    TuiCardLarge, 
+    TuiPlatform, 
+    TuiButton, 
+    TuiTextfield, 
+    TuiAppearance, 
+    TuiButtonClose, 
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './new-poll.html',
   styleUrl: './new-poll.less',
 })
 export class NewPoll {
+  protected newPollForm: FormGroup = new FormGroup({
+    option1: new FormControl(""),
+    option2: new FormControl(""),
+  });
+  
+  numberOfOptions = 2;
 
+  public addOption() {
+    this.numberOfOptions += 1;
+    this.newPollForm.addControl(`option${this.numberOfOptions}`, new FormControl<string>("", { nonNullable: true }));
+  }
+
+  public getOptionsKeys(): string[] {
+    console.log(Object.keys(this.newPollForm.controls));
+    return Object.keys(this.newPollForm.controls);
+  }
 }
