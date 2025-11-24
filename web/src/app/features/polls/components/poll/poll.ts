e5c39a28bcd8d708bbbb3bf9b7a4a77858a42f1c
Willian Almeida <willianac01@gmail.com>
2025-11-24 10:28:25 -0300

    feat: use TUI radio component as poll options

--- web/src/app/features/polls/components/poll/poll.ts
+++ web/src/app/features/polls/components/poll/poll.ts
@@ -3,20 +3,17 @@ import { TuiPlatform } from '@taiga-ui/cdk';
 import { TuiButton } from '@taiga-ui/core';
 import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 import { TuiLabel, TuiTitle} from '@taiga-ui/core';
-import { TuiCheckbox } from '@taiga-ui/kit';
+import { TuiRadio } from '@taiga-ui/kit';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-poll',
-  imports: [TuiPlatform, TuiCardLarge, TuiHeader, TuiButton, TuiCheckbox, TuiLabel, TuiTitle, ReactiveFormsModule],
+  imports: [TuiPlatform, TuiCardLarge, TuiHeader, TuiButton, TuiLabel, TuiTitle, ReactiveFormsModule, TuiRadio],
   templateUrl: './poll.html',
   styleUrl: './poll.less',
 })
 export class Poll {
-   protected testForm = new FormGroup({
-        testValue1: new FormControl(false),
-        testValue2: new FormControl(false),
-        testValue3: new FormControl(false),
-    });
-
+  protected pollForm = new FormGroup({
+    option: new FormControl(false)
+  });
 }
