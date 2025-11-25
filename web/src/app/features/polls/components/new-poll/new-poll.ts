0f7f21406e73f0acd23a6797a7e090ea01158077
Willian Almeida <willianac01@gmail.com>
2025-11-25 08:57:59 -0300

    feat: add username when creating new poll

--- web/src/app/features/polls/components/new-poll/new-poll.ts
+++ web/src/app/features/polls/components/new-poll/new-poll.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../../../shared/components/navbar/navbar";
-import { TuiCardLarge } from '@taiga-ui/layout';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 import { TuiPlatform } from '@taiga-ui/cdk';
-import { TuiButton, TuiTextfield, TuiAppearance } from '@taiga-ui/core';
+import { TuiButton, TuiTextfield, TuiAppearance, TuiIcon } from '@taiga-ui/core';
 import { TuiButtonClose } from '@taiga-ui/kit';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -18,7 +18,9 @@ import { CommonModule } from '@angular/common';
     TuiAppearance, 
     TuiButtonClose, 
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    TuiHeader,
+    TuiIcon
   ],
   templateUrl: './new-poll.html',
   styleUrl: './new-poll.less',
