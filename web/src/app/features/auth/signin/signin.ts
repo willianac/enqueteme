2e3975463a91031ffe2e15988138304caa33cc0c
Willian Almeida <willianac01@gmail.com>
2025-12-01 08:58:18 -0300

    feat: sign in page UI

--- web/src/app/features/auth/signin/signin.ts
+++ web/src/app/features/auth/signin/signin.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../../shared/components/navbar/navbar";
+import {TuiButton, TuiTitle, TuiTextfield, TuiLabel} from '@taiga-ui/core';
+import {TuiAvatar} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
-  imports: [Navbar],
+  imports: [Navbar, TuiCardLarge, TuiHeader, TuiTitle, TuiButton, TuiAvatar, TuiTextfield, FormsModule, TuiLabel],
   templateUrl: './signin.html',
   styleUrl: './signin.less',
 })
 export class Signin {
-
+  name = "";
 }
