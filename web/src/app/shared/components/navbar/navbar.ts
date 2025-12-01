60babfc2669e161852f64fcf1fef03910c4e86d5
Willian Almeida <willianac01@gmail.com>
2025-12-01 12:27:23 -0300

    fix: fix UI in mobile devices

--- web/src/app/shared/components/navbar/navbar.ts
+++ web/src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TuiButton, TuiIcon, TuiIcons } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TuiButton, TuiIcon],
+  imports: [TuiButton, TuiIcon, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.less',
 })
