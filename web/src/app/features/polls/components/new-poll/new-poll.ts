80551ec17e3fc9fae556333659c322cd2a09dcd1
Willian Almeida <willianac01@gmail.com>
2025-12-01 14:33:55 -0300

    feat: add new-poll form validation

--- web/src/app/features/polls/components/new-poll/new-poll.ts
+++ web/src/app/features/polls/components/new-poll/new-poll.ts
@@ -1,8 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { Navbar } from "../../../../shared/components/navbar/navbar";
 import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
-import { TuiPlatform } from '@taiga-ui/cdk';
-import { TuiButton, TuiTextfield, TuiAppearance, TuiIcon } from '@taiga-ui/core';
+import { TuiPlatform, TuiValidationError } from '@taiga-ui/cdk';
+import { TuiButton, TuiTextfield, TuiAppearance, TuiIcon, TuiAlertService, TuiError } from '@taiga-ui/core';
 import { TuiButtonClose, TuiSlider, TuiSwitch } from '@taiga-ui/kit';
 
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -27,18 +27,22 @@ import { RouterLink } from '@angular/router';
     TuiIcon,
     TuiSlider,
     TuiSwitch,
-    RouterLink
+    RouterLink,
+    TuiError
   ],
   templateUrl: './new-poll.html',
   styleUrl: './new-poll.less',
 })
 export class NewPoll {
-  pollApi = inject(PollApi);
+  readonly pollApi = inject(PollApi);
+  readonly alerts = inject(TuiAlertService);
 
   protected newPollForm: FormGroup = new FormGroup({
     option1: new FormControl(""),
     option2: new FormControl(""),
   });
+
+  protected cannotCreatePollError: TuiValidationError | null = null;
 
   protected pollTitle: string = "";
   protected pollDuration = 1
@@ -46,6 +50,8 @@ export class NewPoll {
   protected readonly labels: number[] = [1, 2, 3, 4, 5, 6, 7];
   
   numberOfOptions = 2;
+
+  pollWasCreated = false;
 
   public addOption() {
     this.numberOfOptions += 1;
@@ -59,20 +65,41 @@ export class NewPoll {
   public removeOption(e: string) {
     if (this.numberOfOptions > 2) {
       this.newPollForm.removeControl(e);
-      this.numberOfOptions -= 1;
+      this.numberOfOptions -= 1; 
     }
   }
 
+  public onPollCreation() {
+    this.alerts.open('Sua enquete foi criada com sucesso!', { label: 'Criada' }).subscribe();
+    this.pollWasCreated = true;
+  }
+
+  public onPollCreationError(err: any) {
+    this.alerts.open('Houve um erro ao criar sua enquete. Tente novamente mais tarde.', { label: 'Erro' }).subscribe();
+    console.error(err);
+  }
+
+  public isPollCreationAllowed(): boolean {
+    const options = this.newPollForm.getRawValue() as Record<string, string>;
+
+    return Object.values(options).every(option => option.trim() !== "") 
+      && this.pollTitle.trim() !== ""
+      && this.numberOfOptions >= 1
+      && this.numberOfOptions <= 5
+      && !this.pollWasCreated;
+  }
+
   public createPoll() {
+    if(!this.isPollCreationAllowed()) {
+      this.cannotCreatePollError = new TuiValidationError('Por favor, preencha todos os campos para criar a enquete.');
+      return;
+    }
+    this.cannotCreatePollError = null;
     const options = this.newPollForm.getRawValue();
     const title = this.pollTitle;
     this.pollApi.createPoll({ title, options: Object.values(options) }).subscribe({
-      next: (response) => {
-        console.log('Poll created successfully:', response);
-      },
-      error: (error) => {
-        console.error('Error creating poll:', error);
-      }
+      next: () => this.onPollCreation(),
+      error: (err) => this.onPollCreationError(err)
     });
   }
 }
