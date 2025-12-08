1755aec0045815d40886b0221bd033aa8520a830
Willian Almeida <willianac01@gmail.com>
2025-12-08 13:19:28 -0300

    feat: navbar dinamically shows user name

--- web/src/app/shared/components/navbar/navbar.ts
+++ web/src/app/shared/components/navbar/navbar.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TuiButton, TuiIcon, TuiIcons } from '@taiga-ui/core';
+import { UserApi } from '../../../features/auth/services/user-api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TuiButton, TuiIcon, RouterLink],
+  imports: [TuiButton, TuiIcon, RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.less',
 })
 export class Navbar {
-
+  readonly userApi = inject(UserApi);
+  protected user = this.userApi.user();
 }
